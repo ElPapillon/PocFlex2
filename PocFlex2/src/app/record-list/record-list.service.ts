4623b477f7bb62d592b89a2300d9b4f3d0b06836
Lucas DUFOUR <lucas.dufour974@gmail.com>
2019-07-19 09:18:32 +0200

    Ajout du service adjoint record-list

--- PocFlex2/src/app/record-list/record-list.service.ts
+++ PocFlex2/src/app/record-list/record-list.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { RecordService } from '../services/Managers/record.service';
+import { Observable } from 'rxjs';
+import { IRecords } from '../models/interfaces/Record.Model';
+import { AppError } from '../models/classes/AppError';
+import { catchError } from 'rxjs/operators';
+import { LoggerService } from '../services/Managers/logger.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecordListService {
 
-  constructor(public RecordService : RecordService) { }
+  constructor(public RecordService : RecordService, public LoggerService: LoggerService) { }
 
-  public GetRecord () {
-
+  public GetRecord (): Observable<IRecords[] | Observable<AppError>> {
+    return this.RecordService.GetRecords().pipe(
+      catchError((error: AppError) => {
+        this.LoggerService.LogError(error);
+        return Observable.throw(error)
+      }),
+    )
  }
 }
