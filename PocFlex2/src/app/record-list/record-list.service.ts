1b1680463eb37487c92cbf8289a1b7dcdcf23478
Lucas DUFOUR <lucas.dufour974@gmail.com>
2019-07-19 08:58:53 +0200

    commit postpush

--- PocFlex2/src/app/record-list/record-list.service.ts
+++ PocFlex2/src/app/record-list/record-list.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { RecordService } from '../services/Managers/record.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecordListService {
 
-  constructor() { }
+  constructor(public RecordService : RecordService) { }
+
+  public GetRecord () {
+
+ }
 }
